8081185c889b4a2a90de48b028810c346755c5b1
Prabhav Gautam <yuleuuee@gmail.com>
2024-05-29 16:23:17 +0800

    intitial commit

--- src/app/(departments)/pcd/page.tsx
+++ src/app/(departments)/pcd/page.tsx
@@ -3,9 +3,10 @@ import Link from 'next/link';
 
 const PartnerCompanyDepartment = () => {
    return (
-      <div><p>login page</p>
+      <div><p>PCD login page </p>
          <Link href={'/pcd/manager'} className="text-blue-600 hover:text-blue-800">
-            login
+            login universal
+            
          </Link>
       </div>
 
